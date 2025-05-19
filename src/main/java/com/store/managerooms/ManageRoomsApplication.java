370436cec56e64cdc1026b013026b3d987fd2082
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2025-05-19 20:00:31 +0200

    Initial commit

---
+++ src/main/java/com/store/managerooms/ManageRoomsApplication.java
@@ -0,0 +1,13 @@
+package com.store.managerooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageRoomsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageRoomsApplication.class, args);
+    }
+
+}
