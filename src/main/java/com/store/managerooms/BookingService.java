d2e8680e6a2634e658c4c121524b79a685d6bb67
Emelie572 <wahlestedt.emelie@gmail.com>
2025-05-20 19:07:04 +0200

    small fix

--- src/main/java/com/store/managerooms/BookingService.java
+++ src/main/java/com/store/managerooms/BookingService.java
@@ -7,12 +7,15 @@ public interface BookingService {
     public BookingDto bookingToBookingDto(Booking booking);
     public DetailedBookingDto bookingToDetailedBookingDto(Booking booking);
     public Booking bookingDtoToBooking(BookingDto bookingDto);
+    public Booking DetailedBookingDtoToBooking(DetailedBookingDto detailedBookingDto);
 
-    public List<DetailedBookingDto> getAllBookings();
+
+        public List<DetailedBookingDto> getAllBookings();
     public BookingDto saveBooking(BookingDto bookingDto);
     public DetailedBookingDto findBookingById(Long id);
     public void deleteBookingById(Long id);
-
+    public Booking updateBooking(Long id, BookingDto bookingDto);
+    public Booking updateDetailedBooking(Long id, DetailedBookingDto detailedBookingDto);
 
 
     }
