d39ece8acc9ba46cc2fc7e7bbd616547e7ab1ebe
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2025-05-20 16:48:02 +0200

    småfix

--- src/main/java/com/store/managerooms/Controllers/CustomerController.java
+++ src/main/java/com/store/managerooms/Controllers/CustomerController.java
@@ -1,14 +1,31 @@
 package com.store.managerooms.Controllers;
 
+import com.store.managerooms.Models.Customer;
 import com.store.managerooms.Repos.CustomerRepository;
-import org.springframework.stereotype.Controller;
+import jakarta.validation.Valid;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+//gör ev om till controller
+@RestController
 public class CustomerController {
 
-    private final CustomerRepository customerRepository;
+    private final CustomerRepository customerRepo;
+    public CustomerController(CustomerRepository customerRepository) {this.customerRepo = customerRepository;}
 
-    public CustomerController(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
+    @GetMapping("/customers")
+    public String allCustomers() {
+        return customerRepo.findAll().toString();
+    }
+
+    @PostMapping("/addCustomer")
+    public List<Customer> addCustomer(@Valid @RequestBody Customer c) {
+        customerRepo.save(c);
+        return customerRepo.findAll();
     }
 }
--- src/main/java/com/store/managerooms/Repos/CustomerRepository.java
+++ src/main/java/com/store/managerooms/Repos/CustomerRepository.java
@@ -4,4 +4,7 @@ import com.store.managerooms.Models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    //Hibernate-metod
+    public Customer findByEmail(String email);
 }
