453d1819d42e632cf36c60fc7fb82032db6ca57c
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2025-05-21 15:40:28 +0200

    städat

--- src/main/java/com/store/managerooms/Services/CustomerServiceImpl.java
+++ src/main/java/com/store/managerooms/Services/CustomerServiceImpl.java
@@ -20,12 +20,11 @@ public class CustomerServiceImpl implements CustomerService {
     /*
     INTERAKTIONSMETODER
     */
-    //läs ut alla fullständiga kunder från databasen och gör dem till dto:s
+    @Override
     public List<DetailedCustomerDTO> allCustomers() {
         return customerRepo.findAll().stream().map(this::customerToDetailedCustomerDTO).toList();
     }
 
-    //konvertera till customer, spara ner kund i databasen, returnera sparad DTO
     @Override
     public DetailedCustomerDTO addCustomer(DetailedCustomerDTO d) {
         Customer saved = customerRepo.save(detailedCustomerDTOToCustomer(d));
@@ -44,18 +43,15 @@ public class CustomerServiceImpl implements CustomerService {
        OMVANDLINGSMETODER
     */
 
-    //3A. Omvandlar från Entitets-objekt till DTO-objekt för att läsa
-    //Från Kund-objekt till stor kund-DTO
     public DetailedCustomerDTO customerToDetailedCustomerDTO(Customer c) {
         return new DetailedCustomerDTO(c.getId(), c.getFirstName(), c.getLastName(), c.getEmail(), c.getPhone());
     }
 
-    //Från Kund-objekt till liten kund-DTO, anropa i booking sen
+    //TODO: Anropa i booking sen
     public MinimalCustomerDTO customerToDTO(Customer c) {
         return new MinimalCustomerDTO(c.getId(), c.getFirstName(), c.getLastName());
     }
 
-    //3B. Omvandlar från stort DTO-objekt till stort Entitetsobjekt för att kunna skriva
     public Customer detailedCustomerDTOToCustomer(DetailedCustomerDTO d) {
         return new Customer(d.getId(), d.getFirstName(), d.getLastName(), d.getEmail(), d.getPhone());
     }
