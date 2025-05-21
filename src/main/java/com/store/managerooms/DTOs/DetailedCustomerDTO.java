1d143bcb2ba7943887a00d3571ad5401359075f6
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2025-05-21 16:19:01 +0200

    småfix

--- src/main/java/com/store/managerooms/DTOs/DetailedCustomerDTO.java
+++ src/main/java/com/store/managerooms/DTOs/DetailedCustomerDTO.java
@@ -1,5 +1,6 @@
 package com.store.managerooms.DTOs;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,15 @@ import lombok.NoArgsConstructor;
 public class DetailedCustomerDTO {
     private Long id;
 
+    @NotEmpty(message = "First name is mandatory")
     private String firstName;
+
+    @NotEmpty(message = "Last name is mandatory")
     private String lastName;
+
+    @NotEmpty(message = "Email is mandatory")
     private String email;
+
+    @NotEmpty(message = "Phone number is mandatory")
     private String phone;
 }
