ec90673a538b06a7c29f67408b32233afa5215c8
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2025-05-22 16:07:42 +0200

    småfix

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.mysql:mysql-connector-j")
@@ -33,7 +34,6 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springframework.boot:spring-boot-starter-validation:3.0.5")
-
 }
 
 tasks.withType<Test> {
