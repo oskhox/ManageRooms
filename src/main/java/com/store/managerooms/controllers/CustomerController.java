c325301eb71277974c38efc20dc578fcaab8f692
Emelie572 <wahlestedt.emelie@gmail.com>
2025-05-23 15:10:00 +0200

    ändrat till flera customerobjekt

--- src/main/java/com/store/managerooms/controllers/CustomerController.java
+++ src/main/java/com/store/managerooms/controllers/CustomerController.java
@@ -30,14 +30,10 @@ public class CustomerController {
         detailedCustomerDto.setId(Long.parseLong(id));
         return customerService.changeCustomer(detailedCustomerDto);
     }
-}
-/*
+
     //returnerar string med bekräftelse
     @DeleteMapping("/deleteCustomer/{id}")
     public String deleteCustomer(@PathVariable String id) {
         return customerService.deleteCustomer(Long.parseLong(id));
     }
 }
-
-
- */
