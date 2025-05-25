f71139a5a7d884c824470b52a1e45a1ff01f4e0f
Kim Zugic <kim.zugic@live.com>
2025-05-25 15:33:44 +0200

    small UI fixes

--- src/main/resources/static/js/bookingscript.js
+++ src/main/resources/static/js/bookingscript.js
@@ -2,13 +2,9 @@ function showRoom() {
     const customer = document.getElementById('loginForm');
     const room = document.getElementById('registerForm');
 
-    room.classList.remove('animate-out', 'animate-in');
-    customer.classList.remove('animate-in');
-
     customer.classList.add('animate-out');
     customer.style.display = 'none';
     customer.classList.remove('animate-out');
-
     room.style.display = 'block';
     room.classList.add('animate-in');
 
@@ -18,17 +14,11 @@ function showCustomer() {
     const customer = document.getElementById('loginForm');
     const room = document.getElementById('registerForm');
 
-    customer.classList.remove('animate-out', 'animate-in');
-    room.classList.remove('animate-in');
-
     room.classList.add('animate-out');
+    room.style.display = 'none';
+    room.classList.remove('animate-out');
+    customer.style.display = 'block';
+    customer.classList.add('animate-in');
 
-    setTimeout(() => {
-        room.style.display = 'none';
-        room.classList.remove('animate-out');
-
-        customer.style.display = 'block';
-        customer.classList.add('animate-in');
-    }, 300);
 }
 
