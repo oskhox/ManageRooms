6bc68218369645d4f3706f524376e23a5f3ca092
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2025-05-27 09:51:53 +0200

    validation for addCustomer

--- src/main/java/com/store/managerooms/controllers/CustomerController.java
+++ src/main/java/com/store/managerooms/controllers/CustomerController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -16,22 +17,32 @@ public class CustomerController {
 
     @GetMapping("/customer")
     public String allCustomers(Model model) {
+        model.addAttribute("detailedCustomerDto", new DetailedCustomerDto());
         model.addAttribute("allCustomers", customerService.allCustomers());
         return "customer";
     }
 
     @PostMapping("/addCustomer")
-    public String addCustomer(@ModelAttribute DetailedCustomerDto c) {
-        customerService.addCustomer(c);
+    public String addCustomer(@Valid @ModelAttribute DetailedCustomerDto d, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("allCustomers", customerService.allCustomers());
+            return "customer";
+        }
+        customerService.addCustomer(d);
         return "redirect:/customer";
     }
 
     @PostMapping("/changeCustomer")
-    public String changeCustomer(@ModelAttribute DetailedCustomerDto detailedCustomerDto) {
+    public String changeCustomer(@Valid @ModelAttribute DetailedCustomerDto detailedCustomerDto, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("allCustomers", customerService.allCustomers());
+            return "customer";
+        }
         customerService.changeCustomer(detailedCustomerDto);
         return "redirect:/customer";
     }
 
+    //TODO: Add validation
     @PostMapping("/deleteCustomer")
     public String deleteCustomer(@RequestParam("deleteId") String id) {
         customerService.deleteCustomer(Long.parseLong(id));
