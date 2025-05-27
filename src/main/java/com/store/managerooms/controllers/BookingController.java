1e7340d7702d3e95d8835a5d4ae86b3ac53463ac
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2025-05-27 13:41:55 +0200

    fix

--- src/main/java/com/store/managerooms/controllers/BookingController.java
+++ src/main/java/com/store/managerooms/controllers/BookingController.java
@@ -40,7 +40,7 @@ public class BookingController {
             model.addAttribute("roomNumber", "Rumsnummer");
             model.addAttribute("typeOfRoom", "Rumstyp");
             model.addAttribute("totalPeople", "Antal personer");
-            model.addAttribute("availableBeds", "Antal extra sängar");
+            //model.addAttribute("availableBeds", "Antal extra sängar");
             model.addAttribute("bookingDetails", "Bokningsinformation");
         } catch (NoSuchElementException e) {
             model.addAttribute("errorMessage", "Bokning hittades inte.");
--- src/main/java/com/store/managerooms/controllers/RoomController.java
+++ src/main/java/com/store/managerooms/controllers/RoomController.java
@@ -54,58 +54,6 @@ public class RoomController {
         model.addAttribute("labelEndDate", "Slutdatum:");
 
         return "create";
-
     }
-
-
-
-
-
-
-
-
-
-    //    @RequestMapping("rooms")
-//    public List<Room> getAllRooms(){
-//        return roomRepo.findAll();
-//    }
-
-//    @RequestMapping("rooms/add")
-//    public List<Room> addRoom(@RequestParam int roomNumber,
-//                              @RequestParam int beds,
-//                              @RequestParam int extraBedsAvailable){
-//
-//        if(extraBedsAvailable == 1 && beds == 2 ) {
-//            Room doubleRoom = new Room(roomNumber, new RoomType("Double room", beds, extraBedsAvailable));
-//            roomRepo.save(doubleRoom);
-//        }
-//        else if(extraBedsAvailable == 2 && beds == 2 ) {
-//            Room bigDoubleRoom = new Room(roomNumber, new RoomType("Big double room", beds, extraBedsAvailable));
-//            roomRepo.save(bigDoubleRoom);
-//            }
-//        else if(beds == 1 && extraBedsAvailable == 0) {
-//            Room room = new Room(roomNumber, new RoomType("Single room",beds));
-//            roomRepo.save(room);
-//        }
-//        return roomRepo.findAll();
-//    }
-
-    //    @RequestMapping("rooms/search")
-//    public List<Room> getAvailableRooms(@RequestParam int peopleCount, @RequestParam LocalDate start, @RequestParam LocalDate end) {
-//        List<Room> availableRooms = new ArrayList<>();
-//        long idCounter = 1;
-//        for (Room room : roomRepo.findAll()) {
-//            if(bookingRepo.isDateBookedCheckExistingBooking(room.getRoomId(), start,end,idCounter)) {
-//                System.out.println("Room with id" + room.getRoomId() + " is already booked");
-//            } else {
-//                int peopleInRoom = room.getRoomType().getBedCount() + room.getRoomType().getExtraBedsAvailable();
-//                if(peopleInRoom >= peopleCount) {
-//                    availableRooms.add(room);
-//                }
-//            }
-//            idCounter++;
-//            }
-//        return availableRooms;
-//    }
 }
 
