10ccff99be0a4677f402f827ecf90f8a85135c7a
Kim Zugic <kim.zugic@live.com>
2025-05-27 18:29:36 +0200

    tests

--- src/test/java/com/store/managerooms/RoomTest/RoomServiceTest.java
+++ src/test/java/com/store/managerooms/RoomTest/RoomServiceTest.java
@@ -1,25 +1,17 @@
 package com.store.managerooms.RoomTest;
-
 import com.store.managerooms.dtos.RoomDto;
 import com.store.managerooms.models.Room;
 import com.store.managerooms.models.RoomType;
 import com.store.managerooms.repos.BookingRepository;
 import com.store.managerooms.repos.RoomRepo;
-import com.store.managerooms.repos.RoomTypeRepo;
-import com.store.managerooms.services.RoomService;
 import com.store.managerooms.services.impl.RoomServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
 import java.time.LocalDate;
 import java.util.List;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
@@ -30,7 +22,6 @@ public class RoomServiceTest {
 
     @Mock
     private BookingRepository bookingRepository;
-
 
     @Mock
     private RoomRepo roomRepo;
